0cbee008b9a0e58a0cc67482d9e00ef40406d7ba
Peerawut Luengruengroj <pee.luang45@gmail.com>
2015-11-05 20:10:26 +0700

    Update setting and add player class

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Dressing-Princess
-Prog Meth Project
+Description
+	This is project for 2110215, Programming Methodology. This project should be private but due to financial problem, you can see this project as public. Note that you MUST follow instruction below.
+	
+Instruction
+1. Do not folk this project before permission.
+2. If you have committed your code tell me privately.
+3. This project is used for test only. All copyright is reserved to 5731083221-PL and Sasicha only. Do not copy or edit this project before permission.
+
+Developer
+1. Peerawut Luengruengroj
+2. Sasicha Poonpol
--- src/Setting.java
+++ src/Setting.java
@@ -1,29 +1,8 @@
 
 public class Setting {
-	private int width,height;
-	private boolean playSound;
-	public Setting(){
-		width = 640;
-		height = 480;
-		playSound = true;
-	}
-	public int getWidth() {
-		return width;
-	}
-	public void setWidth(int width) {
-		this.width = width;
-	}
-	public int getHeight() {
-		return height;
-	}
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	public boolean isPlaySound() {
-		return playSound;
-	}
-	public void setPlaySound(boolean playSound) {
-		this.playSound = playSound;
-	}
+	public static int width = 640,height=480;
+	public static boolean isPlaySound = true;
+	
+	
 	
 }
