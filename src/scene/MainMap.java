2283cee928518211272bae3b6b3f2641f6103cbb
Peerawut Luengruengroj <pee.luang45@gmail.com>
2015-12-10 16:05:28 +0700

    Add picture

--- src/scene/MainMap.java
+++ src/scene/MainMap.java
@@ -3,46 +3,42 @@ package scene;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.*;
 
 import manager.GameManager;
+import manager.Resource;
 import manager.Setting;
 import player.Player;
 
 public class MainMap extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JButton shop, game;
-	private JLabel playerStatus;
-	private JPanel center, screenAdjust[];
+	private String playerStatus;
+	private BufferedImage bg;
+	
 	public MainMap() {
 		this.setPreferredSize(new Dimension(Setting.screenWidth, Setting.screenHeight));
-		this.setLayout(new BorderLayout());
-		center = new JPanel();
-		center.setLayout(new GridLayout(1, 2));
-		center.setPreferredSize(new Dimension(Setting.screenWidth*6/8, Setting.screenHeight*6/8));
-		screenAdjust = new JPanel[4];
-		for(int i=0;i<4;i++){
-			screenAdjust[i] = new JPanel();		
-		}
-		screenAdjust[0].setPreferredSize(new Dimension(Setting.screenWidth, Setting.screenHeight/8));
-		screenAdjust[1].setPreferredSize(new Dimension(Setting.screenWidth/8, Setting.screenHeight*6/8));
-		screenAdjust[2].setPreferredSize(new Dimension(Setting.screenWidth/8, Setting.screenHeight*6/8));
-		screenAdjust[3].setPreferredSize(new Dimension(Setting.screenWidth, Setting.screenHeight/8));
-		game = new JButton("Let's Play");
+		this.setLayout(null);
+		bg = Resource.getBackgroundImage("img/Background.png");
+		game = new JButton("play");
 		game.setFont(Setting.bigFont);
+		game.setOpaque(false);
+		game.setContentAreaFilled(false);
+		game.setBorderPainted(false);
+		game.setBounds(300,300 , 300, 300);
 		shop = new JButton("Shop");
 		shop.setFont(Setting.bigFont);
-		playerStatus = new JLabel("Hello "+Player.getPlayerName()+", Your score is "+Player.getScore(),SwingConstants.CENTER);
-		playerStatus.setFont(Setting.standardFont);
-		screenAdjust[0].add(playerStatus);
-		center.add(game);
-		center.add(shop);
-		this.add(screenAdjust[0], BorderLayout.NORTH);
-		this.add(screenAdjust[1], BorderLayout.WEST);
-		this.add(screenAdjust[2], BorderLayout.EAST);
-		this.add(screenAdjust[3], BorderLayout.SOUTH);
-		this.add(center, BorderLayout.CENTER);
+		shop.setOpaque(false);
+		shop.setContentAreaFilled(false);
+		shop.setBorderPainted(false);
+		shop.setBounds(700,300 , 300, 300);
+		playerStatus = "Hello "+Player.getPlayerName()+", Your score is "+Player.getScore();
+		this.add(game);
+		this.add(shop);
+		
 		game.addActionListener(new ActionListener() {
 			
 			@Override
@@ -52,5 +48,16 @@ public class MainMap extends JPanel {
 			}
 		});
 		
+	}
+	@Override
+	public void paintComponent(Graphics g){
+		FontMetrics metrics = g.getFontMetrics(Setting.slimBigFont);
+		Rectangle2D rect = metrics.getStringBounds(playerStatus, g);
+		int x = (Setting.screenWidth - (int) rect.getWidth()) / 2;
+		int y = 80;
+		super.paintComponent(g);
+		g.drawImage(bg, 0, 0, null);
+		g.setFont(Setting.slimBigFont);
+		g.drawString(playerStatus, x,y );
 	}
 }
--- src/scene/Prologue.java
+++ src/scene/Prologue.java
@@ -3,39 +3,49 @@ package scene;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.*;
 
 import manager.GameManager;
+import manager.Resource;
 import manager.Setting;
 
 public class Prologue extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private JLabel l = new JLabel("Test");
-	int i=0;
+	private JLabel pageRun;
+	private BufferedImage page[];
+	int i = 1;
+
 	public Prologue() {
 		this.setPreferredSize(new Dimension(Setting.screenWidth, Setting.screenHeight));
 		this.setLayout(new BorderLayout());
-		this.add(l,BorderLayout.NORTH);
+		page = new BufferedImage[4];
+		page[0] = Resource.getBackgroundImage("img/page0.png");
+		page[1] = Resource.getBackgroundImage("img/page1.png");
+		page[2] = Resource.getBackgroundImage("img/page2.png");
+		page[3] = Resource.getBackgroundImage("img/page3.png");
+		pageRun = new JLabel(new ImageIcon(page[0]));
+		this.add(pageRun, BorderLayout.NORTH);
 	}
-	public void playPrologue(){
-		Timer t = new Timer(100, new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				if(i==3){
-					((Timer)e.getSource()).stop();
-					GameManager.runGame();
+
+	public void playPrologue() {
+		synchronized (this) {
+			Timer t = new Timer(1000, new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (i < 4)
+						pageRun.setIcon(new ImageIcon(page[i]));
+					i++;
+					if (i == 5) {
+						((Timer) e.getSource()).stop();
+						GameManager.runGame();
+					}
+
 				}
-				
-			}
-		});
-		t.start();
-		
+			});
+			t.start();
+		}
 	}
-	@Override
-	public void paintComponent(Graphics g){
-		
-	}
+
 }
